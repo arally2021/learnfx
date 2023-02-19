5ae2fb2e446057992df14ad1c1a95b48fc98e341
Great nwaiwu <110269024+arally2021@users.noreply.github.com>
2023-02-19 08:10:18 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!-- chineke -->
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Great Nwaiwu">
+    <meta name="generator" content="">
+    <title>Learn Forex</title>
+      <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/navbar-static/">
+      <link href="index.php" rel="stylesheet">
+      <link href="login.php" rel="stylesheet">
+      <link href="about.php" rel="stylesheet">
+      <link href="style.css" rel="stylesheet">
+      <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+      <link href="./assets/css/style.css" rel="stylesheet">
+      <link href="navbar-top.css" rel="stylesheet">
+  </head>
+  <body>
+<main>
+  <nav class="navbar navbar-dark bg-dark" aria-label="First navbar example">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Learn Forex Trading</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <div class="collapse navbar-collapse" id="navbarsExample01">
+        <ul class="navbar-nav me-auto mb-2">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+            <ul class="dropdown-menu" aria-labelledby="dropdown01">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form>
+          <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <script src="./assets/js/bootstrap.bundle.min.js"></script>
+
